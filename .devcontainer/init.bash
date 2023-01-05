2363efa55876ea2882dbd0783f38c9078d2adc05
TAKE Takashi <nakamizo.takashi@gmail.com>
2023-01-06 01:13:18 +0900

    Update init.bash

--- .devcontainer/init.bash
+++ .devcontainer/init.bash
@@ -4,6 +4,6 @@ echo "" > .env.local
 echo "NOTION_API_SECRET=${NOTION_API_SECRET}" >> .env.local
 echo "DATABASE_ID=${DATABASE_ID}" >> .env.local
 
-npm install --global yarn
+#npm install --global yarn
 
 npm ci
